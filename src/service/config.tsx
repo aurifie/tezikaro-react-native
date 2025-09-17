b810d0f3fec934c783e78127cf2ccebc84ff03a6
aditya m sharma <adityambha1999@gmail.com>
2025-09-17 16:15:07 +0530

    init

---
+++ src/service/config.tsx
@@ -0,0 +1,11 @@
+import { Platform } from "react-native"
+
+// export const BASE_URL = Platform.OS==='android' ? 'http://10.0.2.2:3000/api': 'http://localhost:3000/api'
+// export const SOCKET_URL =  Platform.OS==='android' ? 'http://10.0.2.2:3000': 'http://localhost:3000'
+export const GOOGLE_MAP_API = "AIzaSyBqR8ck9A02QRP7RIIJ1daV93XNRUDt-Mk"
+export const BRANCH_ID ='68c92a727014c68ccc71beec'
+
+// USE YOUR NETWORK IP OR HOSTED URL
+export const BASE_URL = 'http://192.168.29.183:3000/api'
+export const SOCKET_URL = 'http://192.168.29.183:3000'
+
---
+++ src/utils/Scaling.tsx
@@ -0,0 +1,10 @@
+import { Dimensions, Platform } from "react-native";
+
+export const screenWidth: number = Dimensions.get('window').width
+export const screenHeight: number = Dimensions.get('window').height
+
+// export const NoticeHeight = Platform.OS === 'ios' ? screenHeight * 0.15 : screenHeight * 0.07
+
+export const NoticeHeight = Platform.OS === 'ios'
+  ? Math.ceil(screenHeight * 0.15)
+  : Math.ceil(screenHeight * 0.07);
